1fd84d198bf1e916f94b419b00f056b93ab3eb51
andrei.onac <onac.paul.andrei@gmail.com>
2024-07-07 20:38:49 +0300

    Testing only simple svg with media preferences

--- themes/hugo-elate-theme/static/js/main.js
+++ themes/hugo-elate-theme/static/js/main.js
@@ -472,3 +472,19 @@
 
 
 }());
+
+// // select the favicon 👉
+// const faviconEl = document.querySelector('link[rel="icon"]')
+
+// // watch for changes 🕵️
+// const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+// mediaQuery.addEventListener('change', themeChange)
+
+// // listener 👂
+// function themeChange(event) {
+//   if (event.matches) {
+//     faviconEl.setAttribute('href', '/favicon-32x32-dark.png')
+//   } else {
+//     faviconEl.setAttribute('href', '/favicon-32x32.png')
+//   }
+// }
